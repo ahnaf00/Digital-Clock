038e2869d08ec4756b18d0f4e2b46f7ac2c1d768
Ahnaf Anan <aanana0171@gmail.com>
2022-10-12 12:39:37 +0600

    Some Am pr Pm modifications are made again

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const showTime = function getRecenttIME(){
     let hours  = date.getHours()
     let minutes = date.getMinutes()
     let seconds = date.getSeconds()
-    const amPM = hours > 12 ? "PM" : "AM"
+    let  amPM = hours > 12 ? "PM" : "AM"
 
     hours = hours > 12 ? hours - 12 : hours
+    amPM = hours > 12 ? "PM" : "AM"
     formatterHours = hours >= 10 ? hours : "0" + hours
     formatterMinutes = minutes >= 10 ? minutes : "0" + minutes
     formatterSeconds= seconds >= 10 ? seconds : "0" + seconds
-    displayTime.innerHTML =  formatterHours + " : " + formatterMinutes + " : " + seconds + " " +{amPM}
+    displayTime.innerHTML =  formatterHours + " : " + formatterMinutes + " : " + seconds + " " + amPM
     
 }
 
