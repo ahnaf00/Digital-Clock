0b087190594c849bd9df4be2162251b0fd3f0667
Ahnaf Anan <aanana0171@gmail.com>
2022-10-12 14:52:37 +0600

    main.js file modified

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-
 const displayTime = document.querySelector("#clockID")
 const showTime = function getRecenttIME(){
-    let date        = new Date()
-    let hours       = date.getHours()
-    let minutes     = date.getMinutes()
-    let seconds     = date.getSeconds()
-    const amPM          = formatterHours > 12 ? "PM" : "AM"
+    let date = new Date()
+    let hours  = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    const amPM = hours > 12 ? "PM" : "AM"
 
-    hours               = hours > 12 ? hours - 12 : hours
-    formatterHours      = hours >= 10 ? hours : "0" + hours
-    formatterMinutes    = minutes >= 10 ? minutes : "0" + minutes
-    formatterSeconds    = seconds >= 10 ? seconds : "0" + seconds
-
-
-    displayTime.innerHTML =  formatterHours + " : " + formatterMinutes + " : " + seconds + " " + amPM
+    hours = hours > 12 ? hours - 12 : hours
+    formatterHours = hours >= 10 ? hours : "0" + hours
+    formatterMinutes = minutes >= 10 ? minutes : "0" + minutes
+    formatterSeconds= seconds >= 10 ? seconds : "0" + seconds
+    displayTime.innerHTML =  formatterHours + " : " + formatterMinutes + " : " + seconds + amPM
     
 }
 
